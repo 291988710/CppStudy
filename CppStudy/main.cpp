9d864dc70cd5cfa3207c6bbe34a29b6421c0864b
291988710 <291988719@qq.com>
2016-05-09 14:14:13 +0800

    namespace test.
    测试namespace和重载函数的关系。

--- CppStudy/main.cpp
+++ CppStudy/main.cpp
@@ -6,71 +6,42 @@
 * version: V1.0
 ************************************************************************/
 
-#include <windows.h>
 #include <iostream>
+#include <string>
 using namespace std;
 
-DWORD WINAPI Fun1Proc(LPVOID lpParameter);//thread data
-DWORD WINAPI Fun2Proc(LPVOID lpParameter);//thread data
-
-int tickets=100;
-HANDLE g_hEvent;
-
-void main(){
-	HANDLE hThread1; HANDLE hThread2;
-	//创建人工重置事件内核对象 
-	g_hEvent=CreateEvent(NULL,FALSE,FALSE,"tickets");
-	if (g_hEvent) {
-		if (ERROR_ALREADY_EXISTS==GetLastError()) {
-			cout<<"only one instance can run!"<<endl;
-			return;
-		}
-	}
-	SetEvent(g_hEvent);
-	//创建线程 
-	hThread1=CreateThread(NULL,0,Fun1Proc,NULL,0,NULL); 
-	hThread2=CreateThread(NULL,0,Fun2Proc,NULL,0,NULL); 
-	cout<<"11111111111111111111111111111111\n";
-	CloseHandle(hThread1); 
-	CloseHandle(hThread2);
-	//让主线程睡眠4秒 
-	Sleep(4000);
-	//关闭事件对象句柄 
-	CloseHandle(g_hEvent);
-}
-//线程1的入口函数
-DWORD WINAPI Fun1Proc(LPVOID lpParameter)//thread data
+/************************************************************************
+ * namespace decleration
+************************************************************************/
+namespace npA
 {
-	while (true) {
-		WaitForSingleObject(g_hEvent,INFINITE);
-		//ResetEvent(g_hEvent);
-		if (tickets>0) {
-			Sleep(1); 
-			cout<<"thread1 sell ticket :"<<tickets--<<endl;
-			SetEvent(g_hEvent);
-		} else { 
-			SetEvent(g_hEvent);
-			break; 
-		}
-	}
-	return 0;
+	void print(int n) { cout<<"npA\t"<<n<<endl; }
 }
 
-//线程2的入口函数
-DWORD WINAPI Fun2Proc(LPVOID lpParameter)//thread data
+namespace npB
 {
-	while (true) {
-		//请求事件对象
-		WaitForSingleObject(g_hEvent,INFINITE);
-		//ResetEvent(g_hEvent);
-		if (tickets>0) {
-			Sleep(1);
-			cout<<"thread2 sell ticket :"<<tickets--<<endl;
-			SetEvent(g_hEvent);
-		} else {
-			SetEvent(g_hEvent);
-			break; 
-		} 
-	}
+	void print(double d) { cout<<"npB\t"<<d<<endl; }
+}
+
+void print(string s)
+{
+	cout<<"global\t"<<s<<endl;
+}
+
+using namespace npA;
+using namespace npB;
+
+/************************************************************************
+* Title: main
+* Description: 函数入口
+* Param: void
+* Return: int
+************************************************************************/
+int main()
+{
+	print(1);
+	print(2.2);
+	print("this");
+
 	return 0;
 }
